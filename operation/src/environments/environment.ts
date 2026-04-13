6b1ed4bc93846ede9f9b060fa0a250fa793c86d4
vani <vani@123.com>
2026-04-13 11:15:31 +0530

    new copmt

--- operation/src/environments/environment.ts
+++ operation/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   name: 'Default',
   production: false,
-  apiBaseURL: 'http://88.135.73.1:9000/',
+  apiBaseURL: 'http://117.220.199.65:9005/',
   remoteEntryUrl: 'http://localhost:4210/remoteEntry.js',
   standalone: true,
 };
